4c5b462227ae7a2cedcde40af67ea362ca50b291
Surya Prasad Parajapati <Ercssurya2018@gmail.com>
2022-09-26 18:58:46 +0530

    260922

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -21,23 +21,45 @@ export class RegisterComponent implements OnInit {
     
   }
   //here we get the data of form by using Reactive form
+  
   objectdata:any;
+  submitted = false;
   registerForm = new FormGroup({
     First_name:new FormControl('',Validators.required),
     Last_name:new FormControl(''),
     Email:new FormControl(''),
     Password:new FormControl('')
   });
+  ReciveFormData()
+  {
+    this.registerForm = new FormGroup({
+      First_name:new FormControl('',Validators.required),
+      Last_name:new FormControl(''),
+      Email:new FormControl('',[Validators.required,Validators.email,Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]),
+      Password:new FormControl('',[Validators.required,Validators.minLength(6)])
+    });
+  }
   userReg(){
     console.warn(this.registerForm.value);
     this.objectdata=this.registerForm.value;
     this.userdataApi.SaveUserData(this.registerForm.value).subscribe((res:any)=>{
       if(res){
+        this.ReciveFormData();
         this.router.navigate(['/ShowAllData']);
       } 
       console.warn(res);
     })
   }
+  get f() { return this.registerForm.controls; }
+  onSubmit() {
+    this.submitted = true;
+    // stop here if form is invalid
+    if (this.registerForm.invalid) {
+        return;
+    }
+ 
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value))
+}
    //here we write the code for Show error message for First Name
   get FnameValidator()
   {
